09dfb16acbc3b42f44e0f8e7dafe6506a394828b
Felipe Dalbert <felipeoficial195@gmail.com>
2023-06-29 15:57:25 -0300

    feat(site.js): [tests] - faq feature 1.2 - Adicionada funcionalidade para interromper a animação do fechamento do accordion ao clicar no botão "know more".

--- src/scripts/js/index.js
+++ src/scripts/js/index.js
@@ -70,13 +70,19 @@ indicators.forEach((indicator, index) => {
 
 
 const questions = document.querySelectorAll('.line-question')
+const knowMoreBtn = questions[0].parentNode.querySelector('a')
+
 
 questions.forEach(question => {
-    question.addEventListener('click', () =>{
+
+    question.addEventListener('click', (e) =>{
 
         let currentActiveQuestion = question.parentNode.querySelector('.active')
 
         if(currentActiveQuestion){
+
+            if(e.target == knowMoreBtn) return
+
             currentActiveQuestion.classList.remove('active');
 
             if(currentActiveQuestion == question) return
