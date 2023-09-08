bc5611753eed3024ef01740eb0c79c68acae92f0
Felipe Dalbert <felipeoficial195@gmail.com>
2023-09-08 13:33:37 -0300

    fix(location-anchor): adds just '#addres' in value of setAttribute method

--- src/scripts/js/sections/location-anchor.js
+++ src/scripts/js/sections/location-anchor.js
@@ -1,3 +1,3 @@
 const locationAnchor = document.querySelector('[data-url="#contact"]:last-child') 
 
-if(window.innerWidth <= 680) locationAnchor.setAttribute('data-url', 'index.html#address')
+if(window.innerWidth <= 680) locationAnchor.setAttribute('data-url', '#address')
