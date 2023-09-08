30febca7d5a4ad594e6a12bd23ca6675bb9b7f46
Felipe Dalbert <felipeoficial195@gmail.com>
2023-09-08 13:29:12 -0300

    fix(location-anchor): remove 'index.html' from querySelector

--- src/scripts/js/sections/location-anchor.js
+++ src/scripts/js/sections/location-anchor.js
@@ -1,3 +1,3 @@
-const locationAnchor = document.querySelector('[data-url="index.html#contact"]:last-child') 
+const locationAnchor = document.querySelector('[data-url="#contact"]:last-child') 
 
 if(window.innerWidth <= 680) locationAnchor.setAttribute('data-url', 'index.html#address')
